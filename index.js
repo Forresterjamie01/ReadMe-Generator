f0cbc6d394c50362bfdfb07729d7a0a36beedbb9
Forresterjamie01 <Forresterjamie01@yahoo.com>
2021-04-16 04:17:58 -0400

    created then statement for template for readme and added validate function for questions.

--- index.js
+++ index.js
@@ -8,29 +8,33 @@ inquirer.prompt(
             type:'input',
             name: 'title',
             message: 'What is the Project title?',
-            
+            validate: (value)=>{ if(value){return true} else {return 'Please input a value to proceed'}},
         },
         {
             type:'input',
             name: 'installation',
             message: 'How do you install the app?',
+            validate: (value)=>{ if(value){return true} else {return 'Please input a value to proceed'}},
         },
 
         {
             type:'input',
             name: 'usage',
             message: 'What was your usage?',
+            validate: (value)=>{ if(value){return true} else {return 'Please input a value to proceed'}},
         },
         {
             type:'input',
             name: 'contribution',
             message: 'What was your contribution?',
+            validate: (value)=>{ if(value){return true} else {return 'Please input a value to proceed'}},
         },
 
         {
             type:'input',
             name: 'instructions',
             message: 'Test instructions to follow',
+            validate: (value)=>{ if(value){return true} else {return 'Please input a value to proceed'}},
         },
 
         {
@@ -38,23 +42,57 @@ inquirer.prompt(
             name: 'license',
             message: 'What license did you use?',
             choices:['ISC'],
+            validate: (value)=>{ if(value){return true} else {return 'Please input a value to proceed'}},
         },
 
         {
             type:'input',
-            name: 'Github Username:',
-            message: 'git',
+            name: 'git',
+            message: 'Github Username:',
+            validate: (value)=>{ if(value){return true} else {return 'Please input a value to proceed'}},
         },
 
         {
             type:'input',
             name: 'email',
             message: 'E-mail:',
-            choices:['ISC'],
+            validate: (value)=>{ if(value){return true} else {return 'Please input a value to proceed'}},
         },
 
         
 
 
     ]
+).then(({
+    title,
+    installation,
+    usage,
+    contribution,
+    instructions,
+    license,
+    git,
+    email
+
+})=>{
+    const template =`# ${title}
+    *[Installation](#installation)
+    *[Usage](#usage)
+    *[contribution](#contribution)
+    *[instructions](#instructions)
+    *[license](#license)
+    ${installation}
+    ## usage
+    ${usage}
+    ## contribution
+    ${contribution}
+    ## instructions
+    ${instructions}
+    ## license
+    ${license}
+
+    #Contact
+    #Github :${git}
+    #email: ${email} `;
+}
 )
+
